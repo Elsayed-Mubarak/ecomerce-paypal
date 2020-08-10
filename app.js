d003a21d0225cecfa090b7642e09849292e90cbf
unknown <elsayed.mubarak@gmail.com>
2020-08-10 00:41:18 -0700

    after edit node.js version and node_module

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+console.log("...............im on app.js ................")
 
 app.use('/api/v1', paymentRouter);
 
